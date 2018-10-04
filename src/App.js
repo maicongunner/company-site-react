40376cb549717c6bebc317ba94522bd67ea8ba7a
Maicon Domingues <maikonbdomingues@gmail.com>
2018-10-03 22:08:34 -0300

    site feito com react

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import Site from './Site'
+import Rodape from './componentes/Rodape'
+import Cabecalho from './componentes/Cabecalho'
+import Inicio from './componentes/Inicio'
+import Servicos from './componentes/Servicos'
+import Portfolio from './componentes/Portfolio'
+import Precos from './componentes/Precos'
+import Contatos from './componentes/Contatos'
+
+// Area administrativa
+import Admin from './admin/Admin'
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          {/* elemento fixo no site */}
+          <Cabecalho />
+
+          {/* elementos que serao roteados */}
+          <Route path='/' exact component={Inicio} />
+          <Route path='/servicos' component={Servicos} />
+          <Route path='/portfolio' component={Portfolio} />
+          <Route path='/precos' component={Precos} />
+          <Route path='/contato' component={Contatos} />
+
+          {/* tela painel administrativo */}
+          <Route path='/admin' component={Admin} />
+
+          {/* elemento fixo no site */}
+          <Rodape />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ src/admin/Admin.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Admin extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <h2>Painel Administrativo</h2>
+        )
+    }
+}
+
+export default Admin
+
